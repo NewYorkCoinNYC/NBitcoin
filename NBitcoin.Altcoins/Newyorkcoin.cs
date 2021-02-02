1a78e28e340ce150d33ce8cba8e6b5d36181d9df
CryptoLover705 <50221455+CryptoLover705@users.noreply.github.com>
2021-02-02 11:44:42 +0000

    Create Newyorkcoin.cs
    Added NewYorkCoin Mainnet & Testnet

---
+++ NBitcoin.Altcoins/Newyorkcoin.cs
@@ -0,0 +1,317 @@
+﻿using NBitcoin;
+using NBitcoin.DataEncoders;
+using NBitcoin.Protocol;
+using NBitcoin.RPC;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace NBitcoin.Altcoins
+{
+	// Reference: https://github.com/NewYorkCoinNYC/newyorkcoin/blob/master/src/chainparams.cpp
+	public class NewYorkCoin : NetworkSetBase
+	{
+		public static NewYorkCoin Instance { get; } = new NewYorkCoin();
+
+		public override string CryptoCode => "NYC";
+
+		private NewYorkCoin()
+		{
+
+		}
+		public class NycConsensusFactory : ConsensusFactory
+		{
+			private NycConsensusFactory()
+			{
+			}
+			public static NycConsensusFactory Instance { get; } = new NycConsensusFactory();
+
+			public override BlockHeader CreateBlockHeader()
+			{
+				return new NyccoinBlockHeader();
+			}
+			public override Block CreateBlock()
+			{
+				return new NewYorkCoinBlock(new NewYorkCoinBlockHeader());
+			}
+			protected override TransactionBuilder CreateTransactionBuilderCore(Network network)
+			{
+				var txBuilder = base.CreateTransactionBuilderCore(network);
+				txBuilder.StandardTransactionPolicy.MinFee = Money.Coins(1m);
+				return txBuilder;
+			}
+		}
+
+#pragma warning disable CS0618 // Type or member is obsolete
+		public class AuxPow : IBitcoinSerializable
+		{
+			Transaction tx = new Transaction();
+
+			public Transaction Transactions
+			{
+				get
+				{
+					return tx;
+				}
+				set
+				{
+					tx = value;
+				}
+			}
+
+			uint nIndex = 0;
+
+			public uint Index
+			{
+				get
+				{
+					return nIndex;
+				}
+				set
+				{
+					nIndex = value;
+				}
+			}
+
+			uint256 hashBlock = new uint256();
+
+			public uint256 HashBlock
+			{
+				get
+				{
+					return hashBlock;
+				}
+				set
+				{
+					hashBlock = value;
+				}
+			}
+
+			List<uint256> vMerkelBranch = new List<uint256>();
+
+			public List<uint256> MerkelBranch
+			{
+				get
+				{
+					return vMerkelBranch;
+				}
+				set
+				{
+					vMerkelBranch = value;
+				}
+			}
+
+			List<uint256> vChainMerkleBranch = new List<uint256>();
+
+			public List<uint256> ChainMerkleBranch
+			{
+				get
+				{
+					return vChainMerkleBranch;
+				}
+				set
+				{
+					vChainMerkleBranch = value;
+				}
+			}
+
+			uint nChainIndex = 0;
+
+			public uint ChainIndex
+			{
+				get
+				{
+					return nChainIndex;
+				}
+				set
+				{
+					nChainIndex = value;
+				}
+			}
+
+			BlockHeader parentBlock = new BlockHeader();
+
+			public BlockHeader ParentBlock
+			{
+				get
+				{
+					return parentBlock;
+				}
+				set
+				{
+					parentBlock = value;
+				}
+			}
+
+			public void ReadWrite(BitcoinStream stream)
+			{
+				stream.ReadWrite(ref tx);
+				stream.ReadWrite(ref hashBlock);
+				stream.ReadWrite(ref vMerkelBranch);
+				stream.ReadWrite(ref nIndex);
+				stream.ReadWrite(ref vChainMerkleBranch);
+				stream.ReadWrite(ref nChainIndex);
+				stream.ReadWrite(ref parentBlock);
+			}
+		}
+
+		public class NewYorkCoinBlock : Block
+		{
+			public NewYorkCoinBlock(NewYorkCoinBlockHeader header) : base(header)
+			{
+
+			}
+
+			public override ConsensusFactory GetConsensusFactory()
+			{
+				return DogeConsensusFactory.Instance;
+			}
+		}
+		public class NewYorkCoinBlockHeader : BlockHeader
+		{
+			const int VERSION_AUXPOW = (1 << 8);
+
+			AuxPow auxPow = new AuxPow();
+
+			public AuxPow AuxPow
+			{
+				get
+				{
+					return auxPow;
+				}
+				set
+				{
+					auxPow = value;
+				}
+			}
+
+			public override uint256 GetPoWHash()
+			{
+				var headerBytes = this.ToBytes();
+				var h = NBitcoin.Crypto.SCrypt.ComputeDerivedKey(headerBytes, headerBytes, 1024, 1, 1, null, 32);
+				return new uint256(h);
+			}
+
+			public override void ReadWrite(BitcoinStream stream)
+			{
+				base.ReadWrite(stream);
+				if((Version & VERSION_AUXPOW) != 0)
+				{
+					if(!stream.Serializing)
+					{
+						stream.ReadWrite(ref auxPow);
+					}
+				}
+			}
+		}
+#pragma warning restore CS0618 // Type or member is obsolete
+
+		//Format visual studio
+		//{({.*?}), (.*?)}
+		//Tuple.Create(new byte[]$1, $2)
+		//static Tuple<byte[], int>[] pnSeed6_main = null;
+		//static Tuple<byte[], int>[] pnSeed6_test = null;
+		// Not used in NYC: https://github.com/NewYorkCoinNYC/newyorkcoin/blob/master/src/chainparams.cpp#L135
+
+		protected override NetworkBuilder CreateMainnet()
+		{
+			var builder = new NetworkBuilder();
+			builder.SetConsensus(new Consensus()
+			{
+				SubsidyHalvingInterval = 500000,
+				MajorityEnforceBlockUpgrade = 1500,
+				MajorityRejectBlockOutdated = 1900,
+				MajorityWindow = 2000,
+				PowLimit = new Target(new uint256("00000fffffffffffffffffffffffffffffffffffffffffffffffffffffffffff")),
+				PowTargetTimespan = TimeSpan.FromSeconds(2 * 60 * 60),
+				PowTargetSpacing = TimeSpan.FromSeconds(30),
+				PowAllowMinDifficultyBlocks = false,
+				CoinbaseMaturity = 30,
+				//  Not set in reference client, assuming false
+				PowNoRetargeting = false,
+				//RuleChangeActivationThreshold = 6048,
+				//MinerConfirmationWindow = 8064,
+				ConsensusFactory = NycConsensusFactory.Instance,
+				LitecoinWorkCalculation = true,
+				SupportSegwit = false
+			})
+			.SetBase58Bytes(Base58Type.PUBKEY_ADDRESS, new byte[] { 60 })
+			.SetBase58Bytes(Base58Type.SCRIPT_ADDRESS, new byte[] { 22 })
+			.SetBase58Bytes(Base58Type.SECRET_KEY, new byte[] { 188 })
+			.SetBase58Bytes(Base58Type.EXT_PUBLIC_KEY, new byte[] { 0x04, 0x88, 0xb2, 0x1e })
+			.SetBase58Bytes(Base58Type.EXT_SECRET_KEY, new byte[] { 0x04, 0x88, 0xad, 0xe4 })
+			.SetBech32(Bech32Type.WITNESS_PUBKEY_ADDRESS, Encoders.Bech32("nyc"))
+			.SetBech32(Bech32Type.WITNESS_SCRIPT_ADDRESS, Encoders.Bech32("nyc"))
+			.SetMagic(0x1e0ffff0)
+			.SetPort(17020)
+			.SetRPCPort(18823)
+			.SetName("nyc-main")
+			.AddAlias("nyc-mainnet")
+			.AddAlias("newyorkcoin-mainnet")
+			.AddAlias("newyorkcoin-main")
+			.AddDNSSeeds(new[]
+			{
+				new DNSSeedData("nycoin.money", "dnsseed.nycoin.money"),
+				new DNSSeedData("nycoin.community", "dnsseed.nycoin.community")
+			})
+			.AddSeeds(new NetworkAddress[0])
+			.SetGenesis("5597f25c062a3038c7fd815fe46c67dedfcb3c839fbc8e01ed4044540d08fe48");
+			return builder;
+		}
+
+		protected override NetworkBuilder CreateTestnet()
+		{
+			var builder = new NetworkBuilder();
+			builder.SetConsensus(new Consensus()
+			{
+				SubsidyHalvingInterval = 1722,
+				MajorityEnforceBlockUpgrade = 501,
+				MajorityRejectBlockOutdated = 750,
+				MajorityWindow = 1000,
+				PowLimit = new Target(new uint256("00000fffffffffffffffffffffffffffffffffffffffffffffffffffffffffff")),
+				PowTargetTimespan = TimeSpan.FromSeconds(60),
+				PowTargetSpacing = TimeSpan.FromSeconds(60),
+				PowAllowMinDifficultyBlocks = true,
+				CoinbaseMaturity = 30,
+				//  Not set in reference client, assuming false
+				PowNoRetargeting = false,
+				//RuleChangeActivationThreshold = 6048,
+				//MinerConfirmationWindow = 8064,
+				LitecoinWorkCalculation = true,
+				ConsensusFactory = NycConsensusFactory.Instance,
+				SupportSegwit = false
+			})
+			.SetBase58Bytes(Base58Type.PUBKEY_ADDRESS, new byte[] { 113 })
+			.SetBase58Bytes(Base58Type.SCRIPT_ADDRESS, new byte[] { 196 })
+			.SetBase58Bytes(Base58Type.SECRET_KEY, new byte[] { 241 })
+			.SetBase58Bytes(Base58Type.EXT_PUBLIC_KEY, new byte[] { 0x04, 0x88, 0xb2, 0x1e })
+			.SetBase58Bytes(Base58Type.EXT_SECRET_KEY, new byte[] { 0x04, 0x88, 0xad, 0xe4 })
+			.SetBech32(Bech32Type.WITNESS_PUBKEY_ADDRESS, Encoders.Bech32("tnyc"))
+			.SetBech32(Bech32Type.WITNESS_SCRIPT_ADDRESS, Encoders.Bech32("tnyc"))
+			.SetMagic(0xdcb7c1fc)
+			.SetPort(27020)
+			.SetRPCPort(22555)
+		   .SetName("nyc-test")
+		   .AddAlias("nyc-testnet")
+		   .AddAlias("newyorkcoin-test")
+		   .AddAlias("newyorkcoin-testnet")
+		   .AddDNSSeeds(new[]
+		   {
+				new DNSSeedData("jrn.me.uk", "testseed.jrn.me.uk")
+		   })
+		   .AddSeeds(new NetworkAddress[0])
+		   .SetGenesis("010000000000000000000000000000000000000000000000000000000000000000000000696ad20e2dd4365c7459b4a4a5af743d5e92c6da3229e6532cd605f6533f2a5bb9a7f052f0ff0f1ef7390f000101000000010000000000000000000000000000000000000000000000000000000000000000ffffffff1004ffff001d0104084e696e746f6e646fffffffff010058850c020000004341040184710fa689ad5023690c80f3a49c8f13f8d45b8c857fbcbc8bc4a8e4d3eb4b10f4d4604fa08dce601aaf0f470216fe1b51850b4acf21b179c45070ac7b03a9ac00000000");
+			return builder;
+		}
+
+
+		protected override void PostInit()
+		{
+			RegisterDefaultCookiePath("newyorkc");
+		}
+
+	}
+}
